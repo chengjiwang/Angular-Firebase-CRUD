a603e17b34a0fa03f4601008db655aef1b9dd657
chengjiwang <chengji3728@gmail.com>
2016-05-04 21:51:59 +0800

    modify new message bord

--- js/app.js
+++ js/app.js
@@ -3,20 +3,48 @@
 	myApp.controller('MyController', ['$firebaseArray',
 		function($firebaseArray) {
 			var self = this,
-				ref = new Firebase("https://tainanfood-demo.firebaseio.com/");
-	    	self.todoList = $firebaseArray(ref);
+				// ref = new Firebase("https://tainanfood-demo.firebaseio.com/");
+				ref = new Firebase("https://angular-cruddemo.firebaseio.com/");
+
+			// function time(){
+		 //        var date = new Date();
+		 //        var year = date.getFullYear();
+		 //        var month = date.getMonth()+1;
+		 //        var day = date.getDate();
+		 //        var Hours = date.getHours();
+		 //        var Minutes = date.getMinutes();
+		 //        var time =year+"-"+month+"-"+day +" "+Hours+":"+ Minutes;
+		 //        return time;
+		 //    }
+
+				
+	    	self.crudList = $firebaseArray(ref);
+	    	console.log(2);
 			self.addItem = function(){
-				if(self.newItem){
-				   self.todoList.$add({label:self.newItem});
-				   self.newItem = '';
+				console.log(1);
+				if(self.username){
+				   self.crudList.$add({name:self.username , message:self.usermessage ,time:self.time() });
+				   self.username = '';
+				   self.usermessage = '';
 				}  
 			};
+			self.time = function(){
+				var date = new Date();
+		        var year = date.getFullYear();
+		        var month = date.getMonth()+1;
+		        var day = date.getDate();
+		        var Hours = date.getHours();
+		        var Minutes = date.getMinutes();
+		        var time =year+"-"+month+"-"+day +" "+Hours+":"+ Minutes;
+		        return time;
+			};
+			
 			self.deleteItem = function(item){
-				self.todoList.$remove(item);
+				self.crudList.$remove(item);
 			};
 			self.editItem = function(item){
 				item.label = prompt("edit" ,item.label);
-				self.todoList.$save(item);
+				self.crudList.$save(item);
 			}
 		}
 	]);
