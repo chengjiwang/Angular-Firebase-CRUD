dd9e1cc890e6b8ded0dfe8f9a3ba51a2f1b76702
chengjiwang <chengji3728@gmail.com>
2016-05-08 22:07:02 +0800

    add edit&delete function,modify like/unlike function

--- js/app.js
+++ js/app.js
@@ -3,49 +3,47 @@
 	myApp.controller('MyController', ['$firebaseArray',
 		function($firebaseArray) {
 			var self = this,
-				// ref = new Firebase("https://tainanfood-demo.firebaseio.com/");
 				ref = new Firebase("https://angular-cruddemo.firebaseio.com/");
-
-			// function time(){
-		 //        var date = new Date();
-		 //        var year = date.getFullYear();
-		 //        var month = date.getMonth()+1;
-		 //        var day = date.getDate();
-		 //        var Hours = date.getHours();
-		 //        var Minutes = date.getMinutes();
-		 //        var time =year+"-"+month+"-"+day +" "+Hours+":"+ Minutes;
-		 //        return time;
-		 //    }
-
-				
 	    	self.crudList = $firebaseArray(ref);
-	    	console.log(2);
+	    	
 			self.addItem = function(){
-				console.log(1);
+				
 				if(self.username){
-				   self.crudList.$add({name:self.username , message:self.usermessage ,time:self.time() });
+				   self.crudList.$add({ name: self.username , 
+				   					    message: self.usermessage ,
+				   					    time: self.time() ,
+				   					    like: 0,
+				   					    unlike: 0 });
 				   self.username = '';
 				   self.usermessage = '';
 				}  
 			};
 			self.time = function(){
-				var date = new Date();
-		        var year = date.getFullYear();
-		        var month = date.getMonth()+1;
-		        var day = date.getDate();
-		        var Hours = date.getHours();
-		        var Minutes = date.getMinutes();
-		        var time =year+"-"+month+"-"+day +" "+Hours+":"+ Minutes;
+				var date = new Date(),
+					year = date.getFullYear(),
+					month = date.getMonth()+1,
+					day = date.getDate(),
+					Hours = date.getHours(),
+					Minutes = date.getMinutes(),
+					time =year+"-"+month+"-"+day +" "+Hours+":"+ Minutes;
 		        return time;
 			};
-			
+
 			self.deleteItem = function(item){
 				self.crudList.$remove(item);
 			};
 			self.editItem = function(item){
-				item.label = prompt("edit" ,item.label);
+				item.message = prompt("edit" ,item.message);
 				self.crudList.$save(item);
-			}
+			};
+			self.like = function(item){
+				item.like += 1;
+				self.crudList.$save(item);
+			};
+			self.unlike = function(item){
+				item.unlike += 1;
+				self.crudList.$save(item);
+			};
 		}
 	]);
 })();
